1ab2f9da77fa9acf8188f3ef6377ea380b69e824
Juwendays <juwendays@gmail.com>
2021-01-15 00:39:09 +0700

    50

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $request->session()->flush();
+
         return view('home.index');
     }
 
