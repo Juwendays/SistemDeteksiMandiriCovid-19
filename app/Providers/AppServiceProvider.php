b26164f34bb1dd1bf696e3fc5c40c673d1a8107b
Alvin Juwenda <53189901+Juwendays@users.noreply.github.com>
2022-05-31 08:14:15 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if($this->app->environment() == 'production') {
+            \URL::forceScheme('https');
+            \URL::forceRootUrl(\Config::get('app.url'));
+        }
     }
 
     /**
